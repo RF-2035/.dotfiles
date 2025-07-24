d746215f02d379650da4c8ae58b7c66b7a1cc2e1
RF-2035 <qiangtianxing@gmail.com>
2025-07-24 17:46:03 +0800

    fixed windows detection

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -853,7 +853,7 @@ require('lazy').setup({
         -- add frame = "none" to ~/.config/neovide/config.toml to remove the window frame
         vim.o.titlestring = '%t - Neovide'
         -- using adwaita mono as braille fallback (for mini.map)
-        if vim.fn.has 'win32' then
+        if vim.fn.has 'win32' == 1 then
           vim.o.guifont = 'FiraCode Nerd Font,Noto Sans JP:h11'
         else
           vim.o.guifont = 'FiraCode Nerd Font Mono,Adwaita Mono,Noto Sans CJK JP:h11'
